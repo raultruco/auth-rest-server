6828de5074d0bd088f1c9a75b40c859a01de7dca
Raul Foncillas <raul.foncillas@cityscience.com>
2020-05-12 20:20:35 +0100

    Add unit tests for members routes

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
+import assert from 'assert';
 import app from '../src/app.js';
 
 describe('routes', () => {
@@ -7,7 +8,10 @@ describe('routes', () => {
         try {
             await mongoose.connect(
                 process.env.MONGO_URL,
-                { useNewUrlParser: true }
+                {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+                }
             );
             done();
         } catch (err) {
@@ -45,6 +49,110 @@ describe('routes', () => {
                 //   console.log(res.body);
                 // })
                 .expect(200);
+        });
+    });
+
+    describe('members', () => {
+        let accessToken;
+        let memberId;
+        let memberEmail;
+        beforeAll(async () => {
+            await request(app)
+                .post('/api0/auth/login')
+                .send({ email: 'test2@gmail.com', password: '1234567890' })
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(function(res) {
+                     accessToken = res.body.accessToken;
+                })
+                .expect(200);
+        });
+        it('should create a new member', async () => {
+            await request(app)
+                .post('/api0/members')
+                .send({ email: 'test3@gmail.com', password: '1234567890', sex: 1 })
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .expect('Content-Type', /json/)
+                .expect(function(res) {
+                    memberId = res.body.id;
+                    memberEmail = res.body.email;
+                })
+                .expect(200);
+        });
+        it('should update an existing member', async () => {
+            await request(app)
+                .put(`/api0/members/${memberId}`)
+                .send({ email: 'updated_test3@gmail.com', sex: 2 })
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .expect('Content-Type', /json/)
+                .expect(function(res) {
+                    memberEmail = res.body.email;
+                })
+                .expect(200)
+                .then(res => {
+                    assert.equal(res.body.id, memberId);
+                    assert.equal(res.body.email, memberEmail);
+                    assert.equal(res.body.sex, 2);
+                });
+        });
+        it('should get a member by id', async () => {
+            await request(app)
+                .get(`/api0/members/${memberId}`)
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(res => {
+                    assert.equal(res.body.id, memberId);
+                    assert.equal(res.body.email, memberEmail);
+                    assert.equal(res.body.sex, 2);
+                });
+        });
+        it('should get a member by email', async () => {
+            await request(app)
+                .get(`/api0/members/email/${memberEmail}`)
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(res => {
+                    assert.equal(res.body.id, memberId);
+                    assert.equal(res.body.email, memberEmail);
+                    assert.equal(res.body.sex, 2);
+                });
+        });
+        it('should get all members', async () => {
+            await request(app)
+                .get('/api0/members')
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(res => {
+                    assert.ok(Array.isArray(res.body));
+                    assert.equal(res.body.length, 2);
+                });
+        });
+        it('should delete an existing member', async () => {
+            await request(app)
+                .del(`/api0/members/${memberId}`)
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .expect('Content-Type', /json/)
+                .expect(200);
+        });
+        it('should delete all the members', async () => {
+            await request(app)
+                .del('/api0/members')
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(res => {
+                    assert(res.body.deletedCount, 1);
+                });
         });
     });
 
