fbaeb7bd1b651dfd7f1687f1b14fd63adfde39bb
Raul Foncillas <raul.foncillas@cityscience.com>
2020-05-12 10:00:26 +0100

    Improve verifyToken middleware
    - Handle Authorisation and x-access-token headers
    - Bypass (allow) requests without token
    - Copy token contents data into req.member

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,32 +1,45 @@
-import jwt from 'jsonwebtoken';
-import config from 'config';
-import { Member } from 'models';
+import jwt from "jsonwebtoken";
+import config from "config";
+import { Member } from "models";
 
 export const verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+    let token = null;
+    const tokenHeaderKey =
+      req.header("Authorization") || req.headers["x-access-token"];
 
-  if (!token) {
-    return res.status(403).send({ message: "No token provided!" });
-  }
-
-  jwt.verify(token, config.jwtSecret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Unauthorized!" });
+    if (tokenHeaderKey) {
+        req.headers[tokenHeaderKey].replace(/Bearer /g, "");
     }
-    req.userId = decoded.id;
-    next();
-  });
+
+    if (!token) {
+        req.member = null;
+        next();
+    }
+
+    jwt.verify(token, config.jwtSecret, (err, decoded) => {
+        if (err) {
+            next({ status: 401, message: "Unauthorized!", err });
+        }
+        req.member = {
+            id: decoded.id,
+            fullName: decoded.fn,
+            isAdmin: decoded.ia,
+            avatarUrl: decoded.av,
+        };
+        next();
+    });
 };
 
 export const checkDuplicatedUser = (req, res, next) => {
-  return Member.findOne({ email: req.body.email }).then(member => {
-    if (member) {
-      res.status(500).send({ message: 'Member already exists!' });
-    } else {
-      next();
-    }
-  })
-  .catch(err => {
-    next(err);
-  });
+    return Member.findOne({ email: req.body.email })
+        .then((member) => {
+            if (member) {
+                next({ status: 500, message: "Member already exists!" });
+            } else {
+                next();
+            }
+        })
+        .catch((err) => {
+            next({ status: 500, message: "Member already exists!", err });
+        });
 };
