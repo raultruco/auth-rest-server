6b27f146c1e3405c9b1634c40f82cd9d0f8abd82
Raul Foncillas <raul.foncillas@cityscience.com>
2020-05-13 21:11:55 +0100

    Add pagination unit tests

--- test/routes.test.js
+++ test/routes.test.js
@@ -2,6 +2,21 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 import assert from 'assert';
 import app from '../src/app.js';
+import config from '../src/config.js';
+import Member from '../src/models/member.model.js';
+
+const getFixture = () => {
+    let fixture = [], counterString = '0';
+    for(let i = 0; i < 100; i++) {
+        counterString = `${i}`.padStart(3, '0');
+        fixture.push({
+            email: `member.${counterString}@mailinator.com`,
+            password: 'xxxxxxxx',
+            fullName: counterString
+        });
+    }
+    return fixture;
+}
 
 describe('routes', () => {
     beforeAll(async done => {
@@ -10,7 +25,8 @@ describe('routes', () => {
                 process.env.MONGO_URL,
                 {
                     useNewUrlParser: true,
-                    useUnifiedTopology: true
+                    useUnifiedTopology: true,
+                    useFindAndModify: false
                 }
             );
             done();
@@ -66,6 +82,8 @@ describe('routes', () => {
                      accessToken = res.body.accessToken;
                 })
                 .expect(200);
+            await Member.deleteMany({});
+            await Member.insertMany(getFixture());
         });
         it('should create a new member', async () => {
             await request(app)
@@ -123,7 +141,15 @@ describe('routes', () => {
                     assert.equal(res.body.sex, 2);
                 });
         });
-        it('should get all members', async () => {
+        it('should delete an existing member', async () => {
+            await request(app)
+                .del(`/api0/members/${memberId}`)
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .expect('Content-Type', /json/)
+                .expect(200);
+        });
+        it('should get the first page of members when no pagination supplied', async () => {
             await request(app)
                 .get('/api0/members')
                 .set('Accept', 'application/json')
@@ -132,16 +158,88 @@ describe('routes', () => {
                 .expect(200)
                 .then(res => {
                     assert.ok(Array.isArray(res.body));
-                    assert.equal(res.body.length, 2);
+                    assert.equal(res.body.length, config.pageLimit);
+                    assert.equal(res.body[0].email, `member.000@mailinator.com`);
+                    assert.equal(res.body[49].email, `member.049@mailinator.com`);
                 });
         });
-        it('should delete an existing member', async () => {
+        it('should get the second page of members when pagination supplied', async () => {
             await request(app)
-                .del(`/api0/members/${memberId}`)
+                .get('/api0/members')
                 .set('Accept', 'application/json')
                 .set('Authorization', accessToken)
+                .query({ page: 2 })
                 .expect('Content-Type', /json/)
-                .expect(200);
+                .expect(200)
+                .then(res => {
+                    assert.ok(Array.isArray(res.body));
+                    assert.equal(res.body.length, config.pageLimit);
+                    assert.equal(res.body[0].email, `member.050@mailinator.com`);
+                    assert.equal(res.body[49].email, `member.099@mailinator.com`);
+                });
+        });
+        it('should get the first page of members when an invalid pagination is supplied', async () => {
+            await request(app)
+                .get('/api0/members')
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .query({ page: 'invalid' })
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(res => {
+                    assert.ok(Array.isArray(res.body));
+                    assert.equal(res.body.length, config.pageLimit);
+                    assert.equal(res.body[0].email, `member.000@mailinator.com`);
+                    assert.equal(res.body[49].email, `member.049@mailinator.com`);
+                });
+        });
+        it('should sort the members ascending when sorting supplied', async () => {
+            await request(app)
+                .get('/api0/members')
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .query({ sort: '_id' })
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(res => {
+                    assert.ok(Array.isArray(res.body));
+                    assert.equal(res.body.length, config.pageLimit);
+                    for (let i = 0; i < config.pageLimit - 1; i++) {
+                        assert.ok(res.body[i].id < res.body[i + 1].id);
+                    }
+                });
+        });
+        it('should sort the members descending when sorting supplied', async () => {
+            await request(app)
+                .get('/api0/members')
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .query({ sort: '-_id' })
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(res => {
+                    assert.ok(Array.isArray(res.body));
+                    assert.equal(res.body.length, config.pageLimit);
+                    for (let i = 0; i < config.pageLimit - 1; i++) {
+                        assert.ok(res.body[i].id > res.body[i + 1].id);
+                    }
+                });
+        });
+        it('should sort by default the members when an invalid sorting is supplied', async () => {
+            await request(app)
+                .get('/api0/members')
+                .set('Accept', 'application/json')
+                .set('Authorization', accessToken)
+                .query({ sort: 'invalid' })
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(res => {
+                    assert.ok(Array.isArray(res.body));
+                    assert.equal(res.body.length, config.pageLimit);
+                    for (let i = 0; i < config.pageLimit - 1; i++) {
+                        assert.ok(res.body[i].id < res.body[i + 1].id);
+                    }
+                });
         });
         it('should delete all the members', async () => {
             await request(app)
